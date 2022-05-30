245d82b84235f0086bed6f339a15199468a3e78c
tarun aggarwal <tarun@iotric.com>
2022-05-30 21:09:51 +0530

    all

---
+++ app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+
+app.get('/hello', (req, res) => {
+    return res.send("hello world!");
+})
+
+
+app.listen(3000,() => {
+    console.log('example app listning on port 3000');
+})
+
+module.exports.Handler = serverless(app);
